2003a3f3ca69a7012c0ad6c109a6e7aa9668937b
Liu.Yandong.Hanks <liu.yandong.hanks@vertiv.com>
2026-03-02 17:22:58 +0800

    Move the position of the constant

--- src/Runtime/Types/NumberValue.g.cs
+++ src/Runtime/Types/NumberValue.g.cs
@@ -17,9 +17,6 @@ namespace AuroraScript.Runtime.Types
         /// <summary> A numeric value representing -1. </summary>
         public static readonly NumberValue Negative1 = new NumberValue(-1);
 
-        /// <summary> A numeric value representing Not-a-Number (NaN). </summary>
-        public static readonly NumberValue NaN = new NumberValue(double.NaN);
-
         /// <summary> A numeric value representing 0. </summary>
         public static readonly NumberValue Zero = new NumberValue(0);
 
@@ -49,6 +46,21 @@ namespace AuroraScript.Runtime.Types
 
         /// <summary> A numeric value representing 9. </summary>
         public static readonly NumberValue Num9 = new NumberValue(9);
+
+        /// <summary> Represents the value of positive infinity. </summary>
+        internal readonly static NumberValue POSITIVE_INFINITY = new NumberValue(double.PositiveInfinity);
+        /// <summary> Represents the value of negative infinity. </summary>
+        internal readonly static NumberValue NEGATIVE_INFINITY = new NumberValue(double.NegativeInfinity);
+        /// <summary> Represents the 'Not-a-Number' (NaN) value. </summary>
+        internal readonly static NumberValue NaN = new NumberValue(double.NaN);
+        /// <summary> Represents the largest representable numeric value. </summary>
+        internal readonly static NumberValue MAX_VALUE = new NumberValue(double.MaxValue);
+        /// <summary> Represents the smallest representable numeric value. </summary>
+        internal readonly static NumberValue MIN_VALUE = new NumberValue(double.MinValue);
+        /// <summary> Represents the maximum safe integer in AuroraScript (+9,007,199,254,740,991). </summary>
+        internal static readonly NumberValue MAX_SAFE_INTEGER = new NumberValue(+9_007_199_254_740_991);
+        /// <summary> Represents the minimum safe integer in AuroraScript (-9,007,199,254,740,991). </summary>
+        internal static readonly NumberValue MIN_SAFE_INTEGER = new NumberValue(-9_007_199_254_740_991);
 
         /// <summary>
         /// Native implementation for Number.toString().
--- src/Runtime/Types/TypeConstruct/NumberConstructor.cs
+++ src/Runtime/Types/TypeConstruct/NumberConstructor.cs
@@ -8,36 +8,23 @@ namespace AuroraScript.Runtime.Types.TypeConstruct
     /// </summary>
     internal class NumberConstructor : ScriptType
     {
-        /// <summary> Represents the value of positive infinity. </summary>
-        internal readonly static NumberValue POSITIVE_INFINITY = new NumberValue(double.PositiveInfinity);
-        /// <summary> Represents the value of negative infinity. </summary>
-        internal readonly static NumberValue NEGATIVE_INFINITY = new NumberValue(double.NegativeInfinity);
-        /// <summary> Represents the 'Not-a-Number' (NaN) value. </summary>
-        internal readonly static NumberValue NaN = new NumberValue(double.NaN);
-        /// <summary> Represents the largest representable numeric value. </summary>
-        internal readonly static NumberValue MAX_VALUE = new NumberValue(double.MaxValue);
-        /// <summary> Represents the smallest representable numeric value. </summary>
-        internal readonly static NumberValue MIN_VALUE = new NumberValue(double.MinValue);
-        /// <summary> Represents the maximum safe integer in AuroraScript (+9,007,199,254,740,991). </summary>
-        internal static readonly NumberValue MAX_SAFE_INTEGER = new NumberValue(+9_007_199_254_740_991);
-        /// <summary> Represents the minimum safe integer in AuroraScript (-9,007,199,254,740,991). </summary>
-        internal static readonly NumberValue MIN_SAFE_INTEGER = new NumberValue(-9_007_199_254_740_991);
+
 
         /// <summary> The global singleton instance of the Number constructor. </summary>
         internal readonly static NumberConstructor INSTANCE = new NumberConstructor();
 
         internal NumberConstructor() : base("Number", true)
         {
-            Define("MAX_VALUE", MAX_VALUE, writeable: false, enumerable: false);
-            Define("MIN_VALUE", MIN_VALUE, writeable: false, enumerable: false);
+            Define("MAX_VALUE", NumberValue.MAX_VALUE, writeable: false, enumerable: false);
+            Define("MIN_VALUE", NumberValue.MIN_VALUE, writeable: false, enumerable: false);
 
-            Define("MAX_SAFE_INTEGER", MAX_SAFE_INTEGER, writeable: false, enumerable: false);
-            Define("MIN_SAFE_INTEGER", MIN_SAFE_INTEGER, writeable: false, enumerable: false);
+            Define("MAX_SAFE_INTEGER", NumberValue.MAX_SAFE_INTEGER, writeable: false, enumerable: false);
+            Define("MIN_SAFE_INTEGER", NumberValue.MIN_SAFE_INTEGER, writeable: false, enumerable: false);
 
-            Define("NaN", NaN, writeable: false, enumerable: false);
+            Define("NaN", NumberValue.NaN, writeable: false, enumerable: false);
 
-            Define("POSITIVE_INFINITY", POSITIVE_INFINITY, writeable: false, enumerable: false);
-            Define("NEGATIVE_INFINITY", NEGATIVE_INFINITY, writeable: false, enumerable: false);
+            Define("POSITIVE_INFINITY", NumberValue.POSITIVE_INFINITY, writeable: false, enumerable: false);
+            Define("NEGATIVE_INFINITY", NumberValue.NEGATIVE_INFINITY, writeable: false, enumerable: false);
 
             Define("isNaN", new BondingFunction(IS_NAN), writeable: false, enumerable: false);
             Define("isInteger", new BondingFunction(IS_INTEGER), writeable: false, enumerable: false);
